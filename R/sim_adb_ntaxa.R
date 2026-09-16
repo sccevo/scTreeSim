2c86b49510ba2328eb934169435c0a06bdd041dc
Hema Nekkanti <73551848+hemanekkanti@users.noreply.github.com>
2026-09-16 07:25:20 +0200

    update sampling loops to use cpp implementations

--- R/sim_adb_ntaxa.R
+++ R/sim_adb_ntaxa.R
@@ -3,7 +3,7 @@
 #' @param a vector of scale parameters per type
 #' @param b vector of shape parameters per type
 #' @param d vector of death probabilities per type
-#' @param rho sampling probability 
+#' @param rho sampling probability
 #' @param origin_type one of 0,...,n-1 where n is the number of types
 #' @param Xi_as matrix of asymmetric type transition probabilities
 #' @param Xi_s matrix of symmetric type transition probabilities
@@ -11,23 +11,23 @@
 sim_adb_ntaxa_samp <- function(ntaxa, a, b, d = 0, rho = 1, origin_type = 0, Xi_as = matrix(0), Xi_s = matrix(1), collapse = TRUE) {
   # assert that all inputs are correct
   ntypes = length(a)
-  assertthat::assert_that(all(c(length(b) == ntypes, length(d) == ntypes, 
+  assertthat::assert_that(all(c(length(b) == ntypes, length(d) == ntypes,
                                 dim(Xi_as) == c(ntypes, ntypes), dim(Xi_s) == c(ntypes, ntypes),
-                                origin_type %in% c(0:ntypes), 
+                                origin_type %in% c(0:ntypes),
                                 d >= 0, d < 1, rho > 0, rho <= 1)),
               msg = 'The inputs do not have proper dimensions or values. Please check all parameters!')
   assertthat::assert_that(all(sapply(seq(1, ntypes), function(i) {all.equal(sum(Xi_as[i, ]) + sum(Xi_s[i, ]), 1)})),
               msg = 'The transition probabilities do not some to 1. Please check!')
-  
+
   # estimate the number of taxa in the full tree
   nfull = ntaxa / rho
-  
+
   # simulate full tree
-  tree = sim_adb_ntaxa_complete(ntaxa = nfull, a = a, b = b, d = d, origin_type = origin_type, Xi_as = Xi_as, Xi_s = Xi_s)
+  tree = sim_adb_ntaxa_complete_fast(ntaxa = nfull, a = a, b = b, d = d, origin_type = origin_type, Xi_as = Xi_as, Xi_s = Xi_s)
   if (is.null(tree)) {
     return(NULL)
   }
-  
+
   phylogeny = prune_tree(obj = tree, ntips = ntaxa, collapse = collapse)
 
   return(phylogeny)
@@ -49,28 +49,28 @@ sim_adb_ntaxa_samp <- function(ntaxa, a, b, d = 0, rho = 1, origin_type = 0, Xi_
 sim_adb_ntaxa_complete_fast <- function(ntaxa, a, b, d, origin_type = 0,
                                         Xi_as = matrix(0), Xi_s = matrix(0)) {
   raw <- sim_adb_loop_cpp(ntaxa, a, b, d, Xi_as, Xi_s, origin_type)
-  
+
   nodes <- as.data.frame(raw[c("id","height","type","parent","leftchild","rightchild","status")])
   root_edge <- raw$root_edge
-  
+
   if (sum(nodes$status == 1) < ntaxa) {
     message("Too many particles died!")
     return(NULL)
   }
-  
+
   # truncate
   stopping_time <- min(nodes$height[nodes$status == 1])
   nodes$height[nodes$status == 1] <- stopping_time
-  
+
   # build edge matrix from parent column (skip root which has NA parent)
   child_rows <- which(!is.na(nodes$parent))
   edges <- cbind(nodes$parent[child_rows], nodes$id[child_rows])
-  
+
   # edge lengths
   parent_heights <- nodes$height[match(edges[,1], nodes$id)]
   child_heights  <- nodes$height[match(edges[,2], nodes$id)]
   edge_lengths   <- child_heights - parent_heights
-  
+
   # labels
   n_alive <- sum(nodes$status == 1)
   n_dead  <- sum(nodes$status == 0)
@@ -80,26 +80,26 @@ sim_adb_ntaxa_complete_fast <- function(ntaxa, a, b, d, origin_type = 0,
   nodes$label[nodes$status == 1] <- seq_len(n_alive)
   if (n_dead > 0) nodes$label[nodes$status == 0] <- (n_alive+1):Ntip
   nodes$label[nodes$status == 2] <- (Ntip+1):(Ntip+Nnode)
-  
+
   edges_recoded <- matrix(nodes$label[match(as.vector(edges), nodes$id)],
                           nrow = nrow(edges), ncol = 2)
-  
+
   phylo_tree <- list(edge = edges_recoded, edge.length = edge_lengths,
                      Nnode = Nnode,
                      tip.label = as.character(nodes$label[nodes$status %in% c(0,1)]))
   class(phylo_tree) <- "phylo"
-  
+
   tree <- treeio::as.treedata(phylo_tree)
   tree@phylo$root.edge <- root_edge
   tree@phylo$origin    <- stopping_time
-  
+
   types <- dplyr::tibble(
     node   = nodes$label,
     status = nodes$status,
     type   = as.factor(nodes$type)
   ) |> dplyr::arrange(node)
   tree@data <- types
-  
+
   tree
 }
 
@@ -115,33 +115,33 @@ sim_adb_ntaxa_complete_fast <- function(ntaxa, a, b, d, origin_type = 0,
 #' @importFrom magrittr "%>%"
 #' @importFrom stats rgamma runif
 sim_adb_ntaxa_complete <- function(ntaxa, a, b, d, origin_type = 0, Xi_as = matrix(0), Xi_s = matrix(0)) {
-  
+
   # initialize
   edges = matrix(nrow = 0, ncol = 2)
   edge_lengths = c()
   nodes = data.frame(
-    id = integer(0), 
-    height = numeric(0), 
-    type = integer(0), 
-    parent = integer(0), 
-    leftchild = integer(0), 
-    rightchild = integer(0), 
+    id = integer(0),
+    height = numeric(0),
+    type = integer(0),
+    parent = integer(0),
+    leftchild = integer(0),
+    rightchild = integer(0),
     status = integer(0) # 0 = dead, 1 = alive, 2 = divided
   )
-  
+
   # sample the lifetime of the first particle
   root_edge = rgamma(1, shape = b[origin_type + 1], scale = a[origin_type + 1])
   nodes = dplyr::bind_rows(nodes, c(id = 1, height = root_edge, type = origin_type, parent = NA, leftchild = NA, rightchild = NA, status = 1))
   events = nodes
   event_counter = 1
   living = 1
-  
+
   while (living < ntaxa & nrow(events) > 0) {
     if (living %% 10000 == 0) {
       print(paste0(living, ' cells alive'))
     }
     event = as.list(events[1, ]); events = events[-1, ] # look at one event
-    
+
     if (runif(1) < d[event$type + 1]) {
       # particle dies
       nodes[event$id, "status"] = 0
@@ -154,83 +154,83 @@ sim_adb_ntaxa_complete <- function(ntaxa, a, b, d, origin_type = 0, Xi_as = matr
       nodes[event$id, "status"] = 2
       living = living + 1
 
-      if (ncol(Xi_s) == 1) { 
+      if (ncol(Xi_s) == 1) {
         # single-type case
         children_types = rep(origin_type, 2)
       } else {
         # multi-type case: sample types
         children_types = sample_types(parent_type = event$type, Xi_as = Xi_as, Xi_s = Xi_s)
       }
-      
+
       # sample lifetimes and add new nodes
       left_type = children_types[1]
       left_lifetime = rgamma(1, shape = b[left_type + 1], scale = a[left_type + 1])
-      left_node = c(id = left_id, height = event$height + left_lifetime, type = left_type, 
+      left_node = c(id = left_id, height = event$height + left_lifetime, type = left_type,
                     parent = event$id, leftchild = NA, rightchild = NA, status = 1)
       nodes = dplyr::bind_rows(nodes, left_node)
 
       right_type = children_types[2]
       right_lifetime = rgamma(1, shape = b[right_type + 1], scale = a[right_type + 1])
-      right_node = c(id = right_id, height = event$height + right_lifetime, type = right_type, 
+      right_node = c(id = right_id, height = event$height + right_lifetime, type = right_type,
                      parent = event$id, leftchild = NA, rightchild = NA, status = 1)
       nodes = dplyr::bind_rows(nodes, right_node)
-      
+
       # to be processed, sort by height
-      events = dplyr::bind_rows(events, left_node, right_node) 
+      events = dplyr::bind_rows(events, left_node, right_node)
       events = events %>% dplyr::arrange(height)
-      
+
       # add child relationships and append the current edges and edge lengths
       nodes[event$id, c("leftchild", "rightchild")] = c(left_id, right_id)
       edges = rbind(edges, c(event$id, left_id), c(event$id, right_id))
     }
   }
-  
+
   if (sum(nodes$status == 1) < ntaxa) {
     message('Too many particles died! Try another seed or decrease death probability.')
     return(NULL)
   }
-  
+
   # truncate to the minimal height of living particles
   stopping_time = min(nodes[nodes$status == 1, 'height'])
   assertthat::assert_that(stopping_time > max(nodes[nodes$status == 2, 'height'])) # ensure that truncation occurs after any cell division
   nodes[nodes$status == 1, 'height'] = stopping_time # update heights
-  
+
   # calculate edge lengths post-hoc
   edge_lengths = sapply(c(1:nrow(edges)), function(i) {
     nodes[nodes$id == edges[i, 2], 'height'] - nodes[nodes$id == edges[i, 1], 'height'] })
-  
+
   # calculate number of cells per status
-  Nnode = sum(nodes$status == 2) # number of cell divisions = number of internal nodes 
+  Nnode = sum(nodes$status == 2) # number of cell divisions = number of internal nodes
   n_dead = sum(nodes$status == 0)
   n_alive = sum(nodes$status == 1)
   Ntip = n_dead + n_alive
-  
+
   # assign labels
   nodes[which(nodes$status == 1), 'label'] = c(1:n_alive)
-  if (n_dead > 0) { nodes[which(nodes$status == 0), 'label'] = c((n_alive + 1):Ntip) }  
-  nodes[which(nodes$status == 2), 'label'] = c((Ntip + 1):(Ntip + Nnode)) 
-  
+  if (n_dead > 0) { nodes[which(nodes$status == 0), 'label'] = c((n_alive + 1):Ntip) }
+  nodes[which(nodes$status == 2), 'label'] = c((Ntip + 1):(Ntip + Nnode))
+
   # use labels in edge matrix
   edges_recoded = matrix(nodes[as.double(edges), 'label'], nrow = nrow(edges), ncol = ncol(edges))
-  
+
   # create phylogenetic tree
-  phylo_tree = list(edge = edges_recoded, edge.length = edge_lengths, Nnode = Nnode, 
+  phylo_tree = list(edge = edges_recoded, edge.length = edge_lengths, Nnode = Nnode,
                     tip.label = as.character(nodes[which(nodes$status %in% c(0,1)), 'label']))
   class(phylo_tree) = "phylo"
-  
+
   # create treedata object
   tree = treeio::as.treedata(phylo_tree)
-  tree@phylo$root.edge = root_edge 
+  tree@phylo$root.edge = root_edge
   tree@phylo$origin = stopping_time
   data = tibble::as_tibble(tree)
-  types = nodes %>% 
-    dplyr::select(node = label, status, type) %>% 
+  types = nodes %>%
+    dplyr::select(node = label, status, type) %>%
     dplyr::mutate(type = as.factor(type)) %>%
     dplyr::arrange(node) %>%
     tibble::as_tibble()
   tree@data = types
-  
+
   return(tree)
 }
 
-  
+
--- R/sim_adb_origin.R
+++ R/sim_adb_origin.R
@@ -3,7 +3,7 @@
 #' @param a vector of scale parameters per type
 #' @param b vector of shape parameters per type
 #' @param d vector of death probabilities per type
-#' @param rho sampling probability 
+#' @param rho sampling probability
 #' @param origin_type one of 0,...,n-1 where n is the number of types
 #' @param Xi_as matrix of asymmetric type transition probabilities
 #' @param Xi_s matrix of symmetric type transition probabilities
@@ -12,21 +12,21 @@
 sim_adb_origin_samp <- function(origin_time, a, b, d = 0, rho = 1, origin_type = 0, Xi_as = matrix(0), Xi_s = matrix(1), min_tips = 2, collapse = TRUE) {
   # assert that all inputs are correct
   ntypes = length(a)
-  assertthat::assert_that(all(c(length(b) == ntypes, length(d) == ntypes, 
+  assertthat::assert_that(all(c(length(b) == ntypes, length(d) == ntypes,
                                 dim(Xi_as) == c(ntypes, ntypes), dim(Xi_s) == c(ntypes, ntypes),
                                 origin_time > 0, origin_type %in% c(0:(ntypes-1)),
                                 d >= 0, d < 1, rho > 0, rho <= 1)),
               msg = 'The inputs do not have proper dimensions or values. Please check all parameters!')
   assertthat::assert_that(all(sapply(seq(1, ntypes), function(i) {all.equal(sum(Xi_as[i, ]) + sum(Xi_s[i, ]), 1.)})),
               msg = 'The transition probabilities do not some to 1. Please check!')
-  
+
   # simulate full tree
-  tree = sim_adb_origin_complete(origin_time = origin_time, a = a, b = b, d = d, origin_type = origin_type, Xi_as = Xi_as, Xi_s = Xi_s, min_tips = min_tips)
+  tree = sim_adb_origin_complete_fast(origin_time = origin_time, a = a, b = b, d = d, origin_type = origin_type, Xi_as = Xi_as, Xi_s = Xi_s, min_tips = min_tips)
   if (is.null(tree)) {
     return(NULL)
   }
-  
-  phylo_obj = prune_tree(obj = tree, rho = rho, min_tips = min_tips, collapse = collapse) 
+
+  phylo_obj = prune_tree(obj = tree, rho = rho, min_tips = min_tips, collapse = collapse)
   return(phylo_obj)
 }
 
@@ -43,27 +43,27 @@ sim_adb_origin_samp <- function(origin_time, a, b, d = 0, rho = 1, origin_type =
 #' @param min_tips minimum number of tips in the tree
 #' @export
 sim_adb_origin_complete <- function(origin_time, a, b, d, origin_type = 0, Xi_as = matrix(0), Xi_s = matrix(1), min_tips = 2) {
-  
+
   # initialize
   edges = matrix(nrow = 0, ncol = 2)
   edge_lengths = c()
   nodes = data.frame(
-    id = integer(0), 
-    height = numeric(0), 
-    type = integer(0), 
-    parent = integer(0), 
-    leftchild = integer(0), 
-    rightchild = integer(0), 
+    id = integer(0),
+    height = numeric(0),
+    type = integer(0),
+    parent = integer(0),
+    leftchild = integer(0),
+    rightchild = integer(0),
     status = integer(0) # 0 = dead, 1 = alive, 2 = divided
   )
-  
+
   # sample the lifetime of the first particle
   root_edge = rgamma(1, shape = b[origin_type + 1], scale = a[origin_type + 1])
-  nodes = dplyr::bind_rows(nodes, c(id = 1, height = origin_time - root_edge, type = origin_type, 
+  nodes = dplyr::bind_rows(nodes, c(id = 1, height = origin_time - root_edge, type = origin_type,
                              parent = NA, leftchild = NA, rightchild = NA, status = 1))
   events = nodes
   event_counter = 1
-  
+
   while (nrow(events) > 0) {
     event = as.list(events[1, ]); events = events[-1, ] # look at one event
 
@@ -77,7 +77,7 @@ sim_adb_origin_complete <- function(origin_time, a, b, d, origin_type = 0, Xi_as
       event_counter = event_counter + 2
       nodes[event$id, "status"] = 2
 
-      if (ncol(Xi_s) == 1) { 
+      if (ncol(Xi_s) == 1) {
         # single-type case
         children_types = rep(origin_type, 2)
       } else {
@@ -91,10 +91,10 @@ sim_adb_origin_complete <- function(origin_time, a, b, d, origin_type = 0, Xi_as
       if (event$height - left_lifetime < 0) {
         # censor lifetime
         left_lifetime = event$height
-        left_node = c(id = left_id, height = event$height - left_lifetime, type = left_type, 
+        left_node = c(id = left_id, height = event$height - left_lifetime, type = left_type,
                       parent = event$id, leftchild = NA, rightchild = NA, status = 1)
       } else {
-        left_node = c(id = left_id, height = event$height - left_lifetime, type = left_type, 
+        left_node = c(id = left_id, height = event$height - left_lifetime, type = left_type,
                       parent = event$id, leftchild = NA, rightchild = NA, status = 1)
         events = dplyr::bind_rows(events, left_node) # to be processed
       }
@@ -104,14 +104,14 @@ sim_adb_origin_complete <- function(origin_time, a, b, d, origin_type = 0, Xi_as
       if (event$height - right_lifetime < 0) {
         # censor lifetime, make tip
         right_lifetime = event$height
-        right_node = c(id = right_id, height = event$height - right_lifetime, type = right_type, 
+        right_node = c(id = right_id, height = event$height - right_lifetime, type = right_type,
                       parent = event$id, leftchild = NA, rightchild = NA, status = 1)
       } else {
-        right_node = c(id = right_id, height = event$height - right_lifetime, type = right_type, 
+        right_node = c(id = right_id, height = event$height - right_lifetime, type = right_type,
                        parent = event$id, leftchild = NA, rightchild = NA, status = 1)
         events = dplyr::bind_rows(events, right_node) # to be processed
       }
-    
+
       # add child relationships and append the current edges and edge lengths
       nodes = dplyr::bind_rows(nodes, left_node, right_node)
       nodes[event$id, c("leftchild", "rightchild")] = c(left_id, right_id)
@@ -119,7 +119,7 @@ sim_adb_origin_complete <- function(origin_time, a, b, d, origin_type = 0, Xi_as
       edge_lengths = c(edge_lengths, left_lifetime, right_lifetime)
     }
   }
-  
+
   # calculate number of cells per status
   Nnode = sum(nodes$status == 2)
   n_dead = sum(nodes$status == 0)
@@ -131,37 +131,37 @@ sim_adb_origin_complete <- function(origin_time, a, b, d, origin_type = 0, Xi_as
     message("The simulated tree has too few tips. Try another seed.")
     return(NULL)
   }
-  
+
   # assign labels
   nodes[which(nodes$status == 1), 'label'] = c(1:n_alive)
-  if (n_dead > 0) { nodes[which(nodes$status == 0), 'label'] = c((n_alive + 1):Ntip) }  
-  nodes[which(nodes$status == 2), 'label'] = c((Ntip + 1):(Ntip + Nnode)) 
-  
+  if (n_dead > 0) { nodes[which(nodes$status == 0), 'label'] = c((n_alive + 1):Ntip) }
+  nodes[which(nodes$status == 2), 'label'] = c((Ntip + 1):(Ntip + Nnode))
+
   # use labels in edge matrix
   edges_recoded = matrix(nodes[as.double(edges), 'label'], nrow = nrow(edges), ncol = ncol(edges))
-  
+
   # create phylogenetic tree
-  phylo_tree = list(edge = edges_recoded, edge.length = edge_lengths, Nnode = Nnode, 
+  phylo_tree = list(edge = edges_recoded, edge.length = edge_lengths, Nnode = Nnode,
                     tip.label = as.character(nodes[which(nodes$status %in% c(0,1)), 'label']))
   class(phylo_tree) = "phylo"
-  
+
   # create treedata object
   tree = treeio::as.treedata(phylo_tree)
   tree@phylo$root.edge = root_edge
   tree@phylo$origin = origin_time
   data = tibble::as_tibble(tree)
-  types = nodes %>% 
-    dplyr::select(node = label, status, type) %>% 
+  types = nodes %>%
+    dplyr::select(node = label, status, type) %>%
     dplyr::mutate(type = as.factor(type)) %>%
     dplyr::arrange(node) %>%
     tibble::as_tibble()
   tree@data = types
-  
+
   return(tree)
 }
 
 
-#' R wrapper for a faster C++ implementation of complete Age-Dependent Branching Process 
+#' R wrapper for a faster C++ implementation of complete Age-Dependent Branching Process
 #' for a fixed time interval (since origin)
 #' @param origin_time time of birth of the initial particle
 #' @param a vector of scale parameters per type
@@ -176,43 +176,43 @@ sim_adb_origin_complete_fast <- function(origin_time, a, b, d,
                                          origin_type = 0,
                                          Xi_as = matrix(0), Xi_s = matrix(1),
                                          min_tips = 2) {
-  
+
   raw <- sim_adb_origin_loop_cpp(origin_time, a, b, d, Xi_as, Xi_s,
                                  origin_type)
-  
+
   nodes <- as.data.frame(raw[c("id","height","type","parent","leftchild","rightchild","status")])
   root_edge   <- raw$root_edge
   edges       <- cbind(raw$edges_from, raw$edges_to)
   edge_lengths <- raw$edge_lengths
-  
+
   # calculate number of cells per status
   n_alive <- sum(nodes$status == 1)
   n_dead  <- sum(nodes$status == 0)
   Ntip    <- n_alive + n_dead
   Nnode   <- sum(nodes$status == 2)
-  
+
   # check number of tips
   if (n_alive < min_tips) {
     message("The simulated tree has too few tips. Try another seed.")
     return(NULL)
   }
-  
+
   # assign labels
   nodes$label <- NA_integer_
   nodes$label[nodes$status == 1] <- seq_len(n_alive)
   if (n_dead > 0) nodes$label[nodes$status == 0] <- (n_alive+1):Ntip
   nodes$label[nodes$status == 2] <- (Ntip+1):(Ntip+Nnode)
-  
+
   # use labels in edge matrix
   edges_recoded <- matrix(nodes$label[match(as.vector(edges), nodes$id)],
                           nrow = nrow(edges), ncol = 2)
-  
+
   # creare phylogenetic tree
   phylo_tree <- list(edge = edges_recoded, edge.length = edge_lengths,
                      Nnode = Nnode,
                      tip.label = as.character(nodes$label[nodes$status %in% c(0,1)]))
   class(phylo_tree) <- "phylo"
-  
+
   # create treedata object
   tree <- treeio::as.treedata(phylo_tree)
   tree@phylo$root.edge <- root_edge
@@ -223,7 +223,7 @@ sim_adb_origin_complete_fast <- function(origin_time, a, b, d,
     type   = as.factor(nodes$type)
   ) |> dplyr::arrange(node)
   tree@data <- types
-  
+
   return(tree)
 }
 
